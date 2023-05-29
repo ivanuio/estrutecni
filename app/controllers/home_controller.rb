e32647d0ede14684f34f13be68b979326a129abc
Ivan Ordonez <ivanord@gmail.com>
2023-05-29 11:10:44 -0500

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about #this is about method
+    @about_me = "My name is Paul Ordonez... and Estrutecnia is established from"
+    @answer = 2023-2017 
+  end
+end
