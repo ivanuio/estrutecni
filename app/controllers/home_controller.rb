eea021b9a4b68bc9d9ae05bd1702b2bf7f86bfc2
Ivan Ordonez <ivanord@gmail.com>
2023-06-08 13:24:27 -0500

    navbars working pretty fine

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,16 @@
 class HomeController < ApplicationController
+  require 'date'
   def index
   end
 
   def about #this is about method
-    @about_me = "My name is Paul Ordonez... and Estrutecnia is established from"
-    @answer = 2023-2017 
+    @about_me = "Enhancing the design of a construction project is crucial, not only to meet the client's requirements but also to improve the outcomes for all stakeholders involved in the project-construction process. My name is Paul Ordonez... and Estrutecnia is established"
+    current_year=Date.today.year
+    @answer = current_year -2017 
+  end
+
+  def acerca #this is acerca method
+    @about_me = "Somos un equipo de profesionales comprometidos en brindar soluciones efectivas a nuestros clientes"
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   
   root 'home#index'
-  
+  get 'home/index'
+  get 'home/indice'
   get 'home/about'
-  #get 'home/index'
+  get 'home/acerca'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
